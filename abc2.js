af5af23007f59a8e4afc29ac2e8dbad25628d47b
Mahnoor-Anwer <mahnooranwer13@gmail.com>
2024-06-07 00:20:53 -0700

    first commit

---
+++ abc2.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function hello(name, age, email) {
+    console.log(" my name is ".concat(name, "  my age is ").concat(age, " and my email is ").concat(email, " "));
+}
+hello("Arhama", 20, "Arhama@gmail.com");
+hello("Mahnoor", 21, "Mahnoor@gmail.com");
+hello("saif", 19, "Saif@gmail.com");
+function add(digit1, digit2) {
+    console.log(digit1 + digit2);
+}
+add(67, 3);
